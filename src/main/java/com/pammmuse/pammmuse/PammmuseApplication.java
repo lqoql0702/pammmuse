de25399b28643d94c33ed9db5bb2fb9e4be628bd
inyoung <lqoql0702@gmail.com>
2023-05-26 23:50:39 +0900

    contextLoads() failure 오류 수정

--- src/main/java/com/pammmuse/pammmuse/PammmuseApplication.java
+++ src/main/java/com/pammmuse/pammmuse/PammmuseApplication.java
@@ -3,7 +3,7 @@ package com.pammmuse.pammmuse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class PammmuseApplication {
 
 	public static void main(String[] args) {
